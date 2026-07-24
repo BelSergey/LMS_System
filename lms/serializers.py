a9cf2ce015aa4d28bf4242c30fe7d76db34516d7
Sergey Belyakov <belserg2002@gmail.com>
2026-07-24 13:11:59 +0300

    nest lesson list inside CourseSerializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -8,9 +8,10 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many= True, read_only=True)
     class Meta:
         model = Course
-        fields = ['id', 'title', 'preview', 'description', 'lessons_count']
+        fields = ['id', 'title', 'preview', 'description', 'lessons_count', 'lessons']
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
