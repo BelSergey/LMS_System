a9134e620d8082024a78651cdb827c39b3ad44d0
Sergey Belyakov <belserg2002@gmail.com>
2026-08-09 13:42:45 +0300

    add is_subscribed flag to CourseSerializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -2,18 +2,31 @@ from rest_framework import serializers
 from .models import Course, Lesson
 from .validators import YoutubeLinkValidator
 
+
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
         validators = [YoutubeLinkValidator(field='video_url')]
 
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(many= True, read_only=True)
+    lessons = LessonSerializer(many=True, read_only=True)
+    is_subscribed = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
-        fields = ['id', 'title', 'preview', 'description', 'lessons_count', 'lessons']
+        fields = ['id', 'title', 'preview', 'description', 'lessons_count', 'lessons', 'is_subsrcibed']
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        user = getattr(request, 'user', None)
+
+        if not user or not user.is_authenticated:
+            return False
+
+        return obj.subscriptions.filter(user=user).exists()
