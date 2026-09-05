6298fcb32fc982d2947b5909d16f6ab97ce92380
Sergey Belyakov <belserg2002@gmail.com>
2026-09-05 18:32:16 +0300

    setup deploy: nginx, docker compose, git actions

--- lms/urls.py
+++ lms/urls.py
@@ -1,33 +1,53 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    CourseViewSet, LessonListCreateView, LessonRetrieveUpdateDestroyView,
-    CourseListView, LessonListView, CourseDetailView, LessonDetailView, CourseCreateView, CourseUpdateView,
-    CourseDeleteView, LessonCreateView, LessonUpdateView, LessonDeleteView, SubscriptionAPIView
+    CourseViewSet,
+    LessonListCreateView,
+    LessonRetrieveUpdateDestroyView,
+    CourseListView,
+    LessonListView,
+    CourseDetailView,
+    LessonDetailView,
+    CourseCreateView,
+    CourseUpdateView,
+    CourseDeleteView,
+    LessonCreateView,
+    LessonUpdateView,
+    LessonDeleteView,
+    SubscriptionAPIView,
 )
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet, basename='course')
+router.register(r"courses", CourseViewSet, basename="course")
 
 
-app_name = 'lms'
+app_name = "lms"
 urlpatterns = [
     # API
-    path('', include(router.urls)),
-
-    path('lessons/', LessonListCreateView.as_view(), name='lesson-list-create'),
-    path('lessons/<int:pk>/', LessonRetrieveUpdateDestroyView.as_view(), name='lesson-detail'),
-
-    path('subscribe/', SubscriptionAPIView.as_view(), name='subscription'),
-
-    path('web/courses/', CourseListView.as_view(), name='course_list'),
-    path('web/lessons/', LessonListView.as_view(), name='lesson_list'),
-    path('web/courses/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
-    path('web/lessons/<int:pk>/', LessonDetailView.as_view(), name='lesson_detail'),
-    path('web/courses/create/', CourseCreateView.as_view(), name='course_create'),
-    path('web/courses/<int:pk>/update/', CourseUpdateView.as_view(), name='course_update'),
-    path('web/courses/<int:pk>/delete/', CourseDeleteView.as_view(), name='course_delete'),
-    path('web/lessons/create/', LessonCreateView.as_view(), name='lesson_create'),
-    path('web/lessons/<int:pk>/update/', LessonUpdateView.as_view(), name='lesson_update'),
-    path('web/lessons/<int:pk>/delete/', LessonDeleteView.as_view(), name='lesson_delete'),
+    path("", include(router.urls)),
+    path("lessons/", LessonListCreateView.as_view(), name="lesson-list-create"),
+    path(
+        "lessons/<int:pk>/",
+        LessonRetrieveUpdateDestroyView.as_view(),
+        name="lesson-detail",
+    ),
+    path("subscribe/", SubscriptionAPIView.as_view(), name="subscription"),
+    path("web/courses/", CourseListView.as_view(), name="course_list"),
+    path("web/lessons/", LessonListView.as_view(), name="lesson_list"),
+    path("web/courses/<int:pk>/", CourseDetailView.as_view(), name="course_detail"),
+    path("web/lessons/<int:pk>/", LessonDetailView.as_view(), name="lesson_detail"),
+    path("web/courses/create/", CourseCreateView.as_view(), name="course_create"),
+    path(
+        "web/courses/<int:pk>/update/", CourseUpdateView.as_view(), name="course_update"
+    ),
+    path(
+        "web/courses/<int:pk>/delete/", CourseDeleteView.as_view(), name="course_delete"
+    ),
+    path("web/lessons/create/", LessonCreateView.as_view(), name="lesson_create"),
+    path(
+        "web/lessons/<int:pk>/update/", LessonUpdateView.as_view(), name="lesson_update"
+    ),
+    path(
+        "web/lessons/<int:pk>/delete/", LessonDeleteView.as_view(), name="lesson_delete"
+    ),
 ]
--- users/forms.py
+++ users/forms.py
@@ -8,7 +8,15 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ("first_name",'last_name','phone', 'city', "email", "password1", "password2")
+        fields = (
+            "first_name",
+            "last_name",
+            "phone",
+            "city",
+            "email",
+            "password1",
+            "password2",
+        )
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -25,7 +33,8 @@ class EmailAuthenticationForm(AuthenticationForm):
 class PasswordResetRequestForm(forms.Form):
     email = forms.EmailField(label="Email")
 
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'phone', 'city', 'avatar']
+        fields = ["first_name", "last_name", "phone", "city", "avatar"]
